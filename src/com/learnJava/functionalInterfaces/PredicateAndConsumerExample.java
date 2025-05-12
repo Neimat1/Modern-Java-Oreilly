277fec240d0572a7274ec646a601bb3cf4fb3855
Neimat1 <neimat.soliman.ismail@gmail.com>
2025-05-12 18:30:44 +0300

    apply BiPredicate

--- src/com/learnJava/functionalInterfaces/PredicateAndConsumerExample.java
+++ src/com/learnJava/functionalInterfaces/PredicateAndConsumerExample.java
@@ -5,6 +5,7 @@ import com.learnJava.data.StudentDataBase;
 
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -22,6 +23,10 @@ public class PredicateAndConsumerExample {
     Predicate<Student> isGpaAtLeast3_9 = student -> student.getGpa() >= 3.9;
 
 
+    //use bi-predicate to check if student is in grade level >= 3 and gpa >= 3.9
+    BiPredicate<Integer, Double> isGradeLevelAndGpaBiPredicate = (gradeLevel, gpa) ->
+            gradeLevel >= 3 && gpa >= 3.9;
+
     //print student's name and activities
     BiConsumer<String, List<String>> printStudentNameAndActivities = (name, activities) -> {
         System.out.println(name + " : " + activities);
@@ -30,7 +35,9 @@ public class PredicateAndConsumerExample {
 
     //print student's name and activities based on filter( grade level >= 3 and gpa >= 3.9)
     Consumer<Student> printStudentWithSpecifications = student -> {
-        if (isGradeLevelAtLeast3.and(isGpaAtLeast3_9).test(student))
+//        if (isGradeLevelAtLeast3.and(isGpaAtLeast3_9).test(student))
+        //using bi-predicate
+        if (isGradeLevelAndGpaBiPredicate.test(student.getGradeLevel(), student.getGpa()))
             printStudentNameAndActivities.accept(student.getName(), student.getActivities());
     };
 
