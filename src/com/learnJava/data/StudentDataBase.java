99ec8c3ff987025f834fdf85b39ef54229461f35
Neimat1 <neimat.soliman.ismail@gmail.com>
2025-05-12 17:01:09 +0300

    update StudentDataBase

--- src/com/learnJava/data/StudentDataBase.java
+++ src/com/learnJava/data/StudentDataBase.java
@@ -12,20 +12,20 @@ public class StudentDataBase {
         /**
          * 2nd grade student
          */
-        Student student1 = new Student("Nemo", 2, 2, "female", List.of("Reading", "Maths"));
-        Student student2 = new Student("Kenny", 2, 2.5, "male", List.of("Reading", "Maths"));
+        Student student1 = new Student("Nemo", 2, 3, "female", List.of("Reading", "Maths"));
+        Student student2 = new Student("Kenny", 2, 2, "male", List.of("Reading", "Maths"));
 
         /**
          * 3rd grade student
          */
-        Student student3 = new Student("Jerry", 3, 3.9, "male", List.of("Reading", "Maths"));
+        Student student3 = new Student("Jerry", 3, 2.9, "male", List.of("Reading", "Maths"));
         Student student4 = new Student("Tom", 3, 3.9, "male", List.of("Reading", "Maths"));
 
         /**
          * 4th grade student
          */
         Student student5 = new Student("Mickey", 4, 4, "male", List.of("Reading", "Maths"));
-        Student student6 = new Student("Donald", 4, 34, "male", List.of("Reading", "Maths"));
+        Student student6 = new Student("Donald", 4, 3.4, "male", List.of("Reading", "Maths"));
 
         List<Student> students = Arrays.asList(student1, student2, student3, student4, student5, student6);
         return students;
